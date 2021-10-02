608648e929431cefe9cb7ca9327ea1a16df9a62e
Andrew Klenner <atklenner@gmail.com>
2021-10-02 13:23:19 -0700

    Make the paint grid change colors

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-let gridSize = 4;
+let gridSize = 16;
 
 const canvas = document.querySelector('.paint-grid');
 
 for(let i = 1; i <= gridSize * gridSize; i++) {
   const box = document.createElement('div');
+  box.classList.add('tile');
+  box.addEventListener('mouseenter', colorChange);
   canvas.appendChild(box);
+}
+
+function colorChange(element) {
+  element.target.style.background = 'black';
 }
