0a7dcd0aa3a0eafebe16c2c8f689c8de119a67f7
Andrew Klenner <atklenner@gmail.com>
2021-10-02 19:51:52 -0700

    Finish grid size slider

--- script.js
+++ script.js
@@ -1,22 +1,39 @@
 let gridSize = 16;
+let paintColor = 'black';
+let backgroundColor = 'white';
 
-const canvas = document.querySelector('.paint-grid');
-
-for(let i = 1; i <= gridSize * gridSize; i++) {
-  const box = document.createElement('div');
-  box.addEventListener('mouseenter', colorChange);
-  canvas.appendChild(box);
+function draw() {
+  const canvas = document.querySelector('.paint-grid');
+  while(canvas.firstChild) {
+    canvas.removeChild(canvas.firstChild);
+  }
+  //canvas.display.grid = `repeat(${gridSize}, auto) / repeat(${gridSize}, auto)`;
+  for(let i = 1; i <= gridSize * gridSize; i++) {
+    const box = document.createElement('div');
+    box.addEventListener('mouseenter', colorChange);
+    canvas.appendChild(box);
+  }
 }
 
 function colorChange(element) {
-  element.target.style.background = 'black';
+  element.target.style.background = paintColor;
 }
 
 const clear = document.querySelector('#clear');
-const boxes = document.querySelectorAll('div.paint-grid>div');
-
 clear.addEventListener('click', clearPaint);
 
 function clearPaint() {
-  boxes.forEach((box) => box.style.background = 'white');
+  const boxes = document.querySelectorAll('div.paint-grid>div');
+  boxes.forEach((box) => box.style.background = backgroundColor);
 }
+
+const sizeSlider = document.querySelector('#slider');
+sizeSlider.addEventListener('mouseup', changeGridSize);
+
+function changeGridSize() {
+  console.log(sizeSlider.value);
+  gridSize = sizeSlider.value;
+  draw();
+}
+
+draw();
