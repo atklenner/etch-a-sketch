664841bf8b2240b8fa4f9ebe30d14cba7115486b
Andrew Klenner <atklenner@gmail.com>
2021-10-02 17:53:17 -0700

    Add clear button

--- script.js
+++ script.js
@@ -4,11 +4,19 @@ const canvas = document.querySelector('.paint-grid');
 
 for(let i = 1; i <= gridSize * gridSize; i++) {
   const box = document.createElement('div');
-  box.classList.add('tile');
   box.addEventListener('mouseenter', colorChange);
   canvas.appendChild(box);
 }
 
 function colorChange(element) {
   element.target.style.background = 'black';
+}
+
+const clear = document.querySelector('#clear');
+const boxes = document.querySelectorAll('div.paint-grid>div');
+
+clear.addEventListener('click', clearPaint);
+
+function clearPaint() {
+  boxes.forEach((box) => box.style.background = 'white');
 }
